244314d179a03e3e0ba8941c789c2266c45f6101
owner <canerduru@ymail.com>
2021-01-17 13:46:19 +0300

    first

--- timerforme/ViewController.swift
+++ timerforme/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var timer = Timer()
+    var kalanzaman = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        kalanzaman = 15
+        timeLabel.text = "Zaman: \(kalanzaman)"
+        
+  
+     
+        
     }
 
-
+    @IBAction func timeButtton(_ sender: Any) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerfunc), userInfo: nil, repeats: true)
+        
+        
+        
+       
+        
+    }
+    
+    @objc func timerfunc() {
+        //baslata tiklaninca once labeli guncelleyelim
+        timeLabel.text = "Zaman: \(kalanzaman)"
+        kalanzaman = kalanzaman - 1
+        
+        if kalanzaman == 0 {
+            timeLabel.text = "sure bitti"
+            timer.invalidate()
+            kalanzaman = 15
+        }
+        
+    }
 }
 
